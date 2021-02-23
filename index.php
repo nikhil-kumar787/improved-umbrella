c80bcc3d3d77244d2cdf6618f6bc09fb2d019376
Nikhil <nikhilkumar7167@gmail.com>
2021-02-23 13:15:54 +0530

    Added Grid Game

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2048_Grid_Game</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="game.js" charset="utf-8"></script>
+</head>
+<body>
+     <div class="score-container">
+         <div class="score-title">score</div>
+         <span id="score">0</span>
+     </div>
+     <div id="result"></div>
+    <div class="grid"></div>
+</body>
+</html>
